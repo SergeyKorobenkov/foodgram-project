83c2431be94da226e01721fb25f484d5dca90455
Rafaari <skorobenkov@yandex.ru>
2020-09-22 16:30:09 +0000

    version with docker, 1 try

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -91,10 +91,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': env.db(),
 }
 
 # Password validation
